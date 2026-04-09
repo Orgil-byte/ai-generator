a7a78b51a9dbd348250dd107ca737e08302f6569
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-04-09 11:53:48 +0800

    A

--- app/_components/imageAnalysis.tsx
+++ app/_components/imageAnalysis.tsx
@@ -1,27 +1,45 @@
-import { Dispatch, SetStateAction } from "react";
-import { Sparkles, FileText, RefreshCw } from "lucide-react";
+"use client";
+
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Sparkles, FileText, RefreshCw, LoaderCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import { ChatCompletionOutputMessage } from "@huggingface/tasks";
 
 type ImageAnalysisProps = {
   file: File | null;
   setFile: Dispatch<SetStateAction<File | null>>;
-  //   analyzeImage: () => Promise<void>;
-  result: string | null;
-  setResult: Dispatch<SetStateAction<string | null>>;
+  analyzeImage: () => Promise<void>;
+  result: ChatCompletionOutputMessage | null;
+  setResult: Dispatch<SetStateAction<ChatCompletionOutputMessage | null>>;
+  preview: string;
+  setPreview: Dispatch<SetStateAction<string>>;
+  loadingAnalyze: boolean;
 };
 
 export const ImageAnalysis = ({
   file,
   setFile,
-  //   analyzeImage,
+  analyzeImage,
   result,
   setResult,
+  preview,
+  setPreview,
+  loadingAnalyze,
 }: ImageAnalysisProps) => {
   const handleReset = () => {
-    setFile(null);
     setResult(null);
+    setFile(null);
+    setPreview("");
   };
+
+  useEffect(() => {
+    if (file && file.type.startsWith("image/")) {
+      const url = URL.createObjectURL(file);
+
+      setPreview(url);
+    }
+  }, [file]);
 
   return (
     <div className="max-w-2xl space-y-6">
@@ -44,17 +62,22 @@ export const ImageAnalysis = ({
         <p className="text-sm text-muted-foreground">
           Upload a food photo, and AI will detect the items.
         </p>
-        <input
-          type="file"
-          accept=".jpg,.jpeg,.png"
-          onChange={(e) => setFile(e.target.files?.[0] ?? null)}
-          className="w-full border border-input rounded-lg px-4 py-2 text-sm text-muted-foreground cursor-pointer file:mr-3 file:font-medium file:text-foreground file:bg-transparent file:border-0 file:cursor-pointer"
-        />
+        <div>
+          {preview === "" ? (
+            <input
+              type="file"
+              accept=".jpg,.jpeg,.png"
+              onChange={(e) => setFile(e.target.files?.[0] ?? null)}
+              className="w-full border border-input rounded-lg px-4 py-2 text-sm text-muted-foreground cursor-pointer file:mr-3 file:font-medium file:text-foreground file:bg-transparent file:border-0 file:cursor-pointer"
+            />
+          ) : (
+            <img src={preview} alt="Image" className="object-cover" />
+          )}
+        </div>
         <div className="flex justify-end">
           <Button
             variant="outline"
-            // onClick={analyzeImage}
-            disabled={!file}
+            onClick={analyzeImage}
             className="px-6 cursor-pointer hover:bg-black hover:text-white hover:border-white transition-colors"
           >
             Generate
@@ -63,21 +86,28 @@ export const ImageAnalysis = ({
       </div>
 
       <div className="space-y-3">
-        <div className="flex items-center gap-2">
-          <FileText className="w-5 h-5" />
-          <h2 className="text-xl font-semibold">Here is the summary</h2>
-        </div>
+        {loadingAnalyze ? (
+          <div className="w-full flex justify-center">
+            I am analyzing the image...
+            <LoaderCircle className="animate-spin" />
+          </div>
+        ) : (
+          <div className="flex items-center gap-2">
+            <FileText className="w-5 h-5" />
+            <h2 className="text-xl font-semibold">Here is the summary</h2>
+          </div>
+        )}
         {result ? (
-          <Card className="rounded-xl border border-border">
+          <Card className="rounded-xl border border-zinc-400">
             <CardContent className="p-4">
-              <p className="text-sm leading-relaxed">{result}</p>
+              <p className="text-sm leading-relaxed">{result?.content}</p>
             </CardContent>
           </Card>
-        ) : (
+        ) : !loadingAnalyze ? (
           <p className="text-sm text-muted-foreground">
             First, enter your image to recognize an items.
           </p>
-        )}
+        ) : null}
       </div>
     </div>
   );
--- app/pageClient.tsx
+++ app/pageClient.tsx
@@ -5,6 +5,7 @@ import { FoodImageCreator } from "./_components/imageGenerator";
 import { InferenceClient } from "@huggingface/inference";
 import { LoaderCircle } from "lucide-react";
 import { ImageAnalysis } from "./_components/imageAnalysis";
+import { ChatCompletionOutputMessage } from "@huggingface/tasks";
 
 export const MainClient = () => {
   const [activeSection, setActiveSection] = useState("");
@@ -12,9 +13,53 @@ export const MainClient = () => {
   const [image, setImage] = useState("");
   const [loading, setLoading] = useState(false);
   const [file, setFile] = useState<File | null>(null);
-  const [result, setResult] = useState<string | null>("");
+  const [result, setResult] = useState<ChatCompletionOutputMessage | null>(
+    null,
+  );
+  const [preview, setPreview] = useState("");
+  const [loadingAnalyze, setLoadingAnalyze] = useState(false);
 
   const client = new InferenceClient(process.env.NEXT_PUBLIC_API_KEY);
+
+  const analyzeImage = async () => {
+    setLoadingAnalyze(true);
+    if (!file) return;
+
+    const base64 = await new Promise<string>((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const result = reader.result as string;
+        resolve(result);
+      };
+      reader.onerror = reject;
+      reader.readAsDataURL(file);
+    });
+
+    const chatCompletion = await client.chatCompletion({
+      model: "moonshotai/Kimi-K2.5:novita",
+      messages: [
+        {
+          role: "user",
+          content: [
+            {
+              type: "text",
+              text: "Describe this image, tell me all the items you see and if there is any food tell me every ingredients there.",
+            },
+            {
+              type: "image_url",
+              image_url: {
+                url: base64,
+              },
+            },
+          ],
+        },
+      ],
+    });
+
+    const aiAnswer = chatCompletion.choices[0].message;
+    setResult(aiAnswer);
+    setLoadingAnalyze(false);
+  };
 
   const fetchImage = async () => {
     setLoading(true);
@@ -29,8 +74,6 @@ export const MainClient = () => {
     setImage(imgUrl);
     setLoading(false);
   };
-
-  const analyzeImage = async () => {};
 
   return (
     <div className="w-full flex justify-center">
@@ -51,6 +94,10 @@ export const MainClient = () => {
             setFile={setFile}
             result={result}
             setResult={setResult}
+            preview={preview}
+            setPreview={setPreview}
+            analyzeImage={analyzeImage}
+            loadingAnalyze={loadingAnalyze}
           />
         ) : null}
         {activeSection === "generator" ? (
